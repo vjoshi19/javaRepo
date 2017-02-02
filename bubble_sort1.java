93360571d5a82ce0a6cfaf2b4e981e1f5809379e
Varsha Joshi <vjoshi19@outlook.com>
2017-02-02 10:26:08 -0800

    added a flag

--- bubble_sort1.java
+++ bubble_sort1.java
@@ -9,32 +9,39 @@ public class bubble_sort1 {
 		int a[] = {5, 1, 6, 2, 4, 3};
 		
 		sort("ascending", a);
-		
 		sort("decending", a);
 		
 		}
 		
 		public static void sort(String wSort, int[] a){
 			int temp;
+			
 			int arrLen = a.length;
 			System.out.println("List has "+ (arrLen - 1) + " elements" );
 			System.out.println("Performing a " + wSort + "sort");
 			for(int i = 0; i < arrLen; i++){
+				int flag = 0;
 				for(int j = 0; j < arrLen-i-1; j++){ // the arrLen -i -1 is to account for n - 1 comparisions for each loop.
 					if (wSort == "ascending"){
 						if (a[j] > a[j+1]){
 							temp = a[j];
 							a[j] = a[j+1];
 							a[j+1] = temp;
+							flag = 1;
 						}
 					}else{
 						if (a[j] < a[j+1]){
 							temp = a[j];
 							a[j] = a[j+1];
 							a[j+1] = temp;
+							flag = 1;
 						}
 					}
 					
+				}
+				if(flag == 0){
+					System.out.println("break");
+					break;
 				}
 				System.out.print(i + "->");
 				for(int k = 0; k < arrLen;  k++){
